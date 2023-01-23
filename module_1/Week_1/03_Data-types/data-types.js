cb0b8e6a839a546fc4cc71ff86bb041c990a75f1
HOME <homegor14@gmail.com>
2023-01-23 18:59:36 +0700

    3. Data-type

---
+++ module_1/Week_1/03_Data-types/data-types.js
@@ -0,0 +1,23 @@
+const age = 20; // Number
+const text = 'Hello' // String
+const boolean = false // Boolean
+const zero = null // null
+let empty ; // Undefined
+const computer = { // Object
+    cpu: 'intel',
+    memory: 32,
+    video: true,
+    mouse: null
+}
+const id = Symbol("id"); //Symbol
+let bigNum = 10n; // BigInt
+
+
+console.log('age', age, typeof age);
+console.log('text', text, typeof text);
+console.log('boolean', boolean, typeof boolean);
+console.log('zero', zero, typeof zero);
+console.log('empty', empty, typeof empty);
+console.log('computer', computer, typeof computer);
+console.log('id', id, typeof id);
+console.log('bigNum', bigNum, typeof bigNum);
