b3c79f9f9129a9fb5ca0bc69d81f78c02e8c3854
HOME <homegor14@gmail.com>
2023-01-27 17:34:29 +0700

    06_base

---
+++ module_1/Week_1/06_base/lesson_3/index.js
@@ -0,0 +1,8 @@
+const question1 = confirm('JavaScript появился в 1995 году?');
+alert(question1 === true ? 'Верно' : 'Правильные ответ: Это верное утверждение')
+
+const question2 = confirm('Спецификация JavaScript называется ECMAScript?');
+alert(question2 === true ? 'Верно' : 'Правильные ответ: Это верное утверждение')
+
+const question3 = confirm('JavaScript был создан за 1 месяц?');
+alert(question3 === false ? 'Верно' : 'Правильные ответ: 10 дней')
