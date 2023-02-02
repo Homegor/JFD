963acc0ceac5d99ae9d1dd762bda552d1bf95b33
HOME <homegor14@gmail.com>
2023-02-03 05:00:08 +0700

    debugger-lesson_4

--- module_1/Week_2/00_Debugger/lesson_4/index.js
+++ module_1/Week_2/00_Debugger/lesson_4/index.js
@@ -1,18 +1,17 @@
 let numbers = [10, 4, 100, -5, 54, 2]
-let numbers1 = [10, 4, 100, -5, 54, 2]
 let sum = 0;
 
 // Через цикл for
 for (let i = 0; i < numbers.length; i += 1) {
-    numbers[i] = numbers[i] ** 3;
-    sum += numbers[i];
+    sum += numbers[i] ** 3;
+    // sum += numbers[i];
     debugger
 }
 console.log(sum);
 
 // Через цикл for of
 sum = 0;
-for (let num of numbers1){ // Во втором цикле numbers принимает первое оконченное значение массива из-за чего число больше
+for (let num of numbers){ // Во втором цикле numbers принимает первое оконченное значение массива из-за чего число больше
     num = num ** 3;
     sum += num;
     debugger
