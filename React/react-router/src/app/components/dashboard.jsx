01d0cf0b85113be454b19c9dd68bc2a579c20d48
HOME <homegor14@gmail.com>
2023-05-21 20:58:24 +0700

    React-Router

---
+++ React/react-router/src/app/components/dashboard.jsx
@@ -0,0 +1,22 @@
+import { Route, Link } from "react-router-dom";
+import Stats from "./stats";
+import Edit from "./edit";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={"/dashboard/edit"}>Edit</Link>
+        </li>
+        <li>
+          <Link to={"/dashboard"}>Dashboard</Link>
+        </li>
+      </ul>
+      <Route exact path="/dashboard/" component={Stats} />
+      <Route path="/dashboard/edit" component={Edit} />
+    </div>
+  );
+};
+
+export default Dashboard;
