f06e53fe0f9d695b5afb7343ec641440a7f2def2
HOME <homegor14@gmail.com>
2023-06-05 18:25:39 +0700

    Формы_2: Expected newline between test and consequent of ternary expression

--- React/fast-company/src/App.js
+++ React/fast-company/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import NavBar from './components/navBar'
-import { Route, Switch } from 'react-router-dom'
+import NavBar from './components/ui/navBar'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Main from './layouts/main'
 import Users from './layouts/users'
 import Login from './layouts/login'
 
 function App() {
-    return (
-        <div>
-            <NavBar />
-            <Switch>
-                <Route path={'/'} exact component={Users} />
-                <Route path={'/users/:userId?'} component={Users} />
-                <Route path={'/main'} component={Main} />
-                <Route path={'/login'} component={Login} />
-            </Switch>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path={'/users/:userId?'} component={Users} />
+        <Route path={'/login/:type?'} component={Login} />
+        <Route path={'/'} exact component={Main} />
+        <Redirect to={'/'} />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
---
+++ React/fast-company/src/components/ui/qualities/qualitiesList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Qualitie from './qualitie'
+
+const QualitiesList = ({ qualities }) => {
+  return (
+    <>
+      {qualities.map((item) => (
+        <Qualitie
+          key={item.name}
+          color={item.color}
+          name={item.name}
+          _id={item._id}
+        />
+      ))}
+    </>
+  )
+}
+
+QualitiesList.propTypes = {
+  qualities: PropTypes.array.isRequired
+}
+
+export default QualitiesList
